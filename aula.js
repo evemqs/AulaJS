91ccd56ebfee97ce631959c3810f3a3fb76c9593
evemqs <everlyn.marques@souunit.com.br>
2024-08-16 17:13:36 -0300

    Aula 16/08

---
+++ aula.js
@@ -0,0 +1,80 @@
+let numero1 = 10
+let numero2 = 5
+
+// Soma
+let soma = numero1 + numero2
+console.log("Soma: ", soma)
+
+// Subtração
+let subt = numero1 - numero2
+console.log("Subtração: ", subt)
+
+// Multiplicação
+let mult = numero1 * numero2
+console.log("Multiplicação: ", mult)
+
+// Divisão
+let divi = numero1 / numero2
+console.log("Divisão: ", divi)
+
+// Exponencial
+console.log("Exponencial de 10: ", Math.pow(numero1, 2))
+console.log("Exponencial de 5: ", numero2**2)
+
+// Resto
+let resto = numero1 % numero2
+console.log("Resto da divisão de 10 por 5: ", resto)
+
+// Incremento
+let inc = 0
+inc ++
+console.log("Inremento de 0 + 1: ", inc)
+
+// Decremento
+let dec = 1
+dec --
+console.log("Decremento de 0 - 1: ", dec)
+
+
+// Exercicio 1
+let number1 = 5
+let number2 = 7
+let number3 = 10
+let atm = number1 + number2 + number3 / 3
+console.log("Aritmética de 5, 7 e 10: ", atm)
+
+// Exercicio 2
+let b = 10
+let h = 25
+let area = b * h
+console.log("A área do retângulo é: ", area)
+
+// Exercicio 3
+let number = 10
+let incr = number++
+let decr = number--
+console.log("O incremento do número 10 é: ", incr, "e o decremento é: ", decr)
+
+// Exercicio 4
+let n1 = 8
+let n2 = 16
+let result = n1 % n2
+console.log("Resto da divisão de 8 por 16: ", result)
+
+// Exercicio 5
+let f = 32
+let c = (f - 32) * (5/9)
+console.log("A temperatura em Celsius para Fahrenheit fica: ", c)
+
+// Exercicio 6
+let numero = 27
+let dobro = numero * 2
+let metade = numero / 2
+console.log("Dobro de 27: ", dobro, "Metade de 27: ", metade)
+
+// Exercicio 7
+let nu1 = 9
+let nu2 = 3
+let exp = nu1**nu2
+
+console.log("52 elevado a 94 é igual a: ", exp)
